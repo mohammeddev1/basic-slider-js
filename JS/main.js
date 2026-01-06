ee371e2500f60e095f2f9e8a7e7af556c75fecb3
mohammeddev1 <mohammedhm.dev@gmail.com>
2026-01-06 13:07:18 +0200

    add project files

---
+++ JS/main.js
@@ -0,0 +1,62 @@
+var listImages = Array.from(document.querySelectorAll(".row .inner img"));
+var mainModalBox = document.getElementById("mainModal");
+var closeModalIcon = document.querySelector(".fa-xmark");
+var arrowRightIcon = document.querySelector(".fa-circle-arrow-right");
+var arrowLeftIcon = document.querySelector(".fa-circle-arrow-left");
+var modalImage = document.getElementById("modalImage");
+
+var clickedImageIndex;
+for (var i = 0; i < listImages.length; i++) {
+  listImages[i].addEventListener("click", function (e) {
+    clickedImageIndex = listImages.indexOf(e.target);
+    hideArrowIcon();
+    var clickedImgSrc = e.target.getAttribute("src");
+    modalImage.setAttribute("src", clickedImgSrc);
+    openModal();
+  });
+}
+
+closeModalIcon.addEventListener("click", function () {
+  closeMOdal();
+});
+
+arrowRightIcon.addEventListener("click", function () {
+  clickedImageIndex++;
+  hideArrowIcon();
+  var clickedImgSrc = listImages[clickedImageIndex].getAttribute("src");
+
+  modalImage.setAttribute("src", clickedImgSrc);
+});
+
+arrowLeftIcon.addEventListener("click", function () {
+  clickedImageIndex--;
+  hideArrowIcon();
+  var clickedImgSrc = listImages[clickedImageIndex].getAttribute("src");
+  modalImage.setAttribute("src", clickedImgSrc);
+});
+
+mainModalBox.addEventListener("click", function (e) {
+  if (e.target != modalImage) {
+    closeMOdal();
+  }
+});
+
+function hideArrowIcon() {
+  if (clickedImageIndex == listImages.length - 1) {
+    arrowRightIcon.classList.add("d-none");
+  } else {
+    arrowRightIcon.classList.remove("d-none");
+  }
+
+  if (clickedImageIndex == 0) {
+    arrowLeftIcon.classList.add("d-none");
+  } else {
+    arrowLeftIcon.classList.remove("d-none");
+  }
+}
+function openModal() {
+  mainModalBox.classList.remove("d-none");
+}
+function closeMOdal() {
+  mainModalBox.classList.add("d-none");
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+## Basic Slider (JavaScript)
+
+This is a simple image gallery   project built with **HTML**, **CSS (Bootstrap + custom styles)**, and **vanilla JavaScript**.  
+The page shows a grid of avatar images; when you click any image it opens in a fullscreen modal with **next** and **previous** arrows so you can slide through the images, and a **close** icon or background click to exit the modal.
+
+### Live Demo (GitHub Pages)
+
+You can view the project online here:
+
+- **GitHub Pages**: [https://mohammeddev1.github.io/basic-slider-js/](https://mohammeddev1.github.io/basic-slider-js/)
+
+ 
+### Features
+
+- **Responsive image grid** using Bootstrap columns.
+- **Click-to-open modal** that shows the selected image.
+- **Next / Previous navigation** through all images in the gallery.
+- **Arrow auto-hide**: left/right arrows are hidden when you reach the first or last image.
+- **Click outside to close**: clicking outside the image closes the modal.
+
+ 
+
+### How to Run Locally
+
+1. **Clone the repo**:
+
+   ```bash
+   git clone https://github.com/mohammeddev1/basic-slider-js.git
+   cd basic-slider-js
+   ```
+
+2. **Open the project**:
+
+   - Simply open `index.html` in your browser, **or**
+   - Use a simple local server (recommended):
+
+   ```bash
+   # Python 3
+   python -m http.server 8000
+   # then open http://localhost:8000 in your browser
+   ```
+
+### Technologies Used
+
+- **HTML5**
+- **CSS3 + Bootstrap**
+- **Vanilla JavaScript**
+
+ 
+
